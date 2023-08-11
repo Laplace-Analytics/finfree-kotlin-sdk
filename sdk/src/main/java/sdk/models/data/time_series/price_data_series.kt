0c5303ae07b1e75e0a36a41c2b6df5cb96e642c9
KeremBalic <kerem_balic@hotmail.com>
2023-08-11 22:06:43 +0300

    price data series and user equity time series added

---
+++ sdk/src/main/java/sdk/models/data/time_series/price_data_series.kt
@@ -0,0 +1,253 @@
+package sdk.models
+
+import sdk.api.StockDataPeriods
+import sdk.models.core.AssetProvider
+import sdk.models.core.SessionProvider
+import sdk.models.core.sessions.DateTime
+import sdk.models.core.sessions.DateTime.Companion.toEpochMilliSecond
+import java.lang.Double.max
+import java.lang.Double.min
+import java.time.Duration
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZoneOffset
+import java.time.temporal.ChronoUnit
+import java.util.*
+
+class PriceDataPoint(index: Int, timeStamp: Long, value: Double, val open: Double?, val low: Double?, val high: Double?, val volume: Int?)
+    : TimeSeriesDataPoint(index, timeStamp, value) {
+
+    override fun toString(): String {
+        val localDateTime: LocalDateTime = time
+        return "$value - ${localDateTime.minute}"
+    }
+
+    override fun toJson(): Map<String, Any> =   mapOf(
+        "timestamp" to time.toEpochMilliSecond(),
+        "index" to index,
+        "close" to value,
+        "open" to (open ?: 0.0),
+        "low" to (low ?: 0.0),
+        "high" to (high ?: 0.0),
+        "volumeto" to (volume ?: 0.0)
+    )
+
+    companion object {
+       fun fromJson(data: Map<String, Any>): PriceDataPoint {
+            val index = data["index"] as Int
+            val timeStamp = (data["timestamp"] as Number).toLong()
+            val value = data["close"] as Double
+            val open = (data["open"] ?: data["close"]) as Double?
+           val low = data["low"] as? Double ?: data["close"] as Double * 0.99
+           val high = data["high"] as? Double ?: data["close"] as Double * 1.01
+            val volume = data["volumeto"] as Int?
+            return PriceDataPoint(index, timeStamp, value, open, low, high, volume)
+        }
+    }
+
+     fun copyWith(index: Int?= null, timeStamp: Long? = null, value: Double? = null, open: Double? = null, low: Double? = null, high: Double? = null, volume: Int? = null): PriceDataPoint {
+        return PriceDataPoint(
+            index ?: this.index,
+            timeStamp ?: time.toEpochMilliSecond(),
+            value ?: this.value,
+            open ?: this.open,
+            low ?: this.low,
+            high ?: this.high,
+            volume ?: this.volume
+        )
+    }
+}
+
+class PriceDataSeries(
+    data: List<PriceDataPoint>,
+    val asset: Asset,
+    val period: StockDataPeriods,
+    previousClose: Double
+) : TimeSeries<PriceDataPoint>(data, previousClose) {
+    init {
+        findMinMaxValues()
+    }
+
+    val region get() = asset.region
+    val assetClass get() = asset.assetClass
+
+    override fun toString(): String {
+        return "${asset.id}, ${asset.region}, $period: ${data.joinToString { it.toString() }}"
+    }
+
+    fun receiveDataPoint(
+        isLiveDataActive: Boolean,
+        sessionProvider: SessionProvider,
+        close: Double,
+        open: Double? = null,
+        low: Double? = null,
+        high: Double? = null,
+        volume: Int? = null
+    ): PriceDataSeries {
+        val date = if (isLiveDataActive) DateTime.now() else DateTime.delayed()
+        if (!sessionProvider.isDuringMarketHours(region, assetClass,date)) {
+            return this
+        }
+        if (data.isNotEmpty()) {
+            val diff = Duration.between(data.last().time, date).toMinutes().toInt()
+            var lastDataPoint = data.last()
+            if (lastDataPoint.time.dayOfMonth != date.dayOfMonth && period == StockDataPeriods.Price1D) {
+                return PriceDataSeries(
+                    mutableListOf(PriceDataPoint(0, date.toEpochMilliSecond(), close, open, low, high, volume)),
+                    asset,
+                    period,
+                    initialValue
+                )
+            } else if (diff >= period.periodMinutes) {
+                return PriceDataSeries(
+                    data + PriceDataPoint(
+                        lastDataPoint.index + (diff / period.periodMinutes),
+                        date.toEpochMilliSecond(),
+                        close,
+                        open ?: close,
+                        low ?: close,
+                        high ?: close,
+                        volume ?: 0
+                    ),
+                    asset,
+                    period,
+                    initialValue
+                )
+            } else {
+                // update last data point
+                lastDataPoint = lastDataPoint.copyWith(
+                    value = close,
+                    low = min(lastDataPoint.low ?: close, low ?: close),
+                    high = max(lastDataPoint.high ?: close, high ?: close),
+                    volume = (lastDataPoint.volume ?: 0) + (volume ?: 0)
+                )
+                return PriceDataSeries(data, asset, period, initialValue)
+            }
+        } else {
+            return PriceDataSeries(
+                mutableListOf(PriceDataPoint(0, date.toEpochMilliSecond(), close, open, low, high, volume)),
+                asset,
+                period,
+                initialValue
+            )
+        }
+    }
+
+       override fun multiplyBy(multiplier: Double, initialValueMultiplier: Double?): PriceDataSeries {
+        if (data.isEmpty()) {
+            return this
+        }
+
+        val newDataPoints = data.map {
+            it.copyWith(
+                value = it.value * multiplier,
+                open = it.open?.let { open -> open * multiplier },
+                low = it.low?.let { low -> low * multiplier },
+                high = it.high?.let { high -> high * multiplier }
+            )
+        }
+
+        return PriceDataSeries(
+            newDataPoints.toList(),
+            asset,
+            period,
+            initialValue * (initialValueMultiplier ?: multiplier)
+        )
+    }
+     override fun divideBy(divider: Double): PriceDataSeries {
+        if (divider == 0.0) {
+            throw IllegalArgumentException("Division by zero")
+        }
+        return multiplyBy(1 / divider)
+    }
+
+     fun operation(baseFunction: (Double, Double) -> Double, other: TimeSeries<TimeSeriesDataPoint>): PriceDataSeries {
+        if (other.data.isEmpty()) {
+            return this
+        }
+
+        val res = mutableListOf<PriceDataPoint>()
+
+        var i = 0
+        var j = 0
+
+         var currI: PriceDataPoint
+         var currJ: TimeSeriesDataPoint
+
+         fun baseOperation(a: Double, b: Double) = baseFunction(a, b)
+
+        while (i < data.size) {
+            currI = data[i]
+            currJ = other.data[j]
+
+            while (j < other.data.size - 1 && other.data[j + 1].time.isBefore(currI.time)) {
+                j++
+                currJ = other.data[j]
+            }
+
+            val newDataPoint = PriceDataPoint(
+                currI.index,
+                currI.time.toEpochMilliSecond(),
+                baseOperation(currI.value, currJ.value),
+                baseOperation(currI.open ?: currI.value, currJ.value),
+                baseOperation(currI.low ?: currI.value, currJ.value),
+                baseOperation(currI.high ?: currI.value, currJ.value),
+                currI.volume
+            )
+
+            res.add(newDataPoint)
+            i++
+        }
+
+        return PriceDataSeries(
+            res,
+            asset,
+            period,
+            baseOperation(initialValue, other.initialValue)
+        )
+    }
+
+     override fun toJson(): Map<String, Any> {
+        return mapOf(
+            "asset_id" to asset.id,
+            "period" to period,
+            "data" to data.map { it.toJson() },
+            "initialValue" to initialValue
+        )
+    }
+
+    // static function to create PriceDataSeries from a Map (similar to Dart's factory constructor)
+    companion object {
+        fun fromJSON(json: Map<String, Any>, assetProvider: AssetProvider): PriceDataSeries {
+            val asset = assetProvider.findById(json["asset_id"] as String)
+                ?: throw IllegalArgumentException("Asset with id ${json["asset_id"]} not found")
+            val data = (json["data"] as List<Map<String, Any>>).map { PriceDataPoint.fromJson(it) }
+            return PriceDataSeries(data, asset, json["period"] as StockDataPeriods, json["initialValue"] as Double)
+        }
+    }
+
+    override fun findMinMaxValues(
+    ) {
+        var minVal = if (data.isNotEmpty()) data.first().value else initialValue
+        var maxVal = minVal
+
+        for (dataPoint in data) {
+            minVal = min(minVal, dataPoint.low ?: dataPoint.value)
+            maxVal = max(maxVal, dataPoint.high ?: dataPoint.value)
+        }
+        minValue = minVal
+        maxValue = maxVal
+    }
+
+     override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is PriceDataSeries) return false
+        if (asset.id != other.asset.id) return false
+        if (region != other.region) return false
+        if (data != other.data) return false
+        if (period != other.period) return false
+        return true
+    }
+
+}
+
---
+++ sdk/src/main/java/sdk/models/data/time_series/user_equity_time_series.kt
@@ -0,0 +1,362 @@
+package sdk.models.data.time_series
+
+import sdk.api.StockDataPeriods
+import sdk.models.*
+import sdk.models.core.SessionProvider
+import sdk.models.core.sessions.DateTime.Companion.toEpochMilliSecond
+import java.time.Duration
+import java.time.ZoneId
+import java.time.ZoneOffset
+import kotlin.math.absoluteValue
+
+
+
+class UserEquityTimeSeries(
+    data: List<EquityDataPoint>,
+    initialValue: Double,
+    var period: StockDataPeriods,
+    var currency: Currency = Currency.tl
+) : TimeSeries<EquityDataPoint>(data, initialValue) {
+
+    fun receiveDataPoint(newData: EquityDataPoint, sessionProvider: SessionProvider): UserEquityTimeSeries {
+        val date = newData.time
+        if (!sessionProvider.isDuringMarketHours(date = date)) {
+            return this
+        }
+        val close = newData.value
+        val aum = newData.assetsUnderManagement
+        return if (data.isNotEmpty()) {
+            val diff = Duration.between(data.last().time, date).toMinutes().absoluteValue
+
+            when {
+                period == StockDataPeriods.Price1D && data.last().time.atZone(ZoneId.systemDefault()).dayOfYear != date.atZone(ZoneId.systemDefault()).dayOfYear -> {
+                    // Data point for a new day, so start a new timeSeries
+                    UserEquityTimeSeries(
+                        listOf(
+                            EquityDataPoint(
+                                index = 0,
+                                timeStamp = date.toEpochMilliSecond(),
+                                value = close,
+                                assetsUnderManagement =aum,
+                                realizedReturn = newData.realizedReturn,
+                                unrealizedReturn = newData.unrealizedReturn
+                            )
+                        ), initialValue,period,  currency
+                    )
+                }
+                period == StockDataPeriods.Price1D && diff >= period.periodMinutes -> {
+                    // Add new data point
+                    UserEquityTimeSeries(
+                        data + listOf(
+                            EquityDataPoint(
+                                index = data.last().index + (diff / period.periodMinutes).toInt(),
+                                timeStamp = date.toEpochMilliSecond(),
+                                value = close,
+                                assetsUnderManagement =aum,
+                                realizedReturn = newData.realizedReturn,
+                                unrealizedReturn = newData.unrealizedReturn
+                            )
+                        ),initialValue, period,  currency
+                    )
+                }
+                else -> {
+                    // Update the last data point
+                    val updatedData = data.dropLast(1) + data.last().copyWith(
+                        value = close,
+                        assetsUnderManagement = aum,
+                        realizedReturn = newData.realizedReturn,
+                        unrealizedReturn = newData.unrealizedReturn
+                    )
+                    UserEquityTimeSeries(updatedData,initialValue, period,  currency)
+                }
+            }
+        } else {
+            UserEquityTimeSeries(
+                listOf(
+                    EquityDataPoint(
+                        index = 0,
+                        timeStamp = date.toEpochMilliSecond(),
+                        value = close,
+                        assetsUnderManagement =aum,
+                        realizedReturn = newData.realizedReturn,
+                        unrealizedReturn = newData.unrealizedReturn
+                    )
+                ), initialValue,period,  currency
+            )
+        }
+    }
+
+    fun multiplyWithPriceDataPoints(priceData: PriceDataSeries): UserEquityTimeSeries {
+        val res = mutableListOf<EquityDataPoint>()
+
+        var i = 0
+        var j = 0
+        var currI: EquityDataPoint
+        var currJ: PriceDataPoint
+
+        while (i < data.size) {
+            currI = data[i]
+            currJ = priceData.data[j]
+
+            while (j < priceData.data.size - 1 && priceData.data[j + 1].time.isBefore(currI.time)) {
+                j++
+                currJ = priceData.data[j]
+            }
+
+            val newDataPoint = EquityDataPoint(
+                index = currI.index,
+                timeStamp = currI.time.toEpochMilliSecond(),
+                assetsUnderManagement = currI.assetsUnderManagement * currJ.value,
+                value = currI.value * currJ.value,
+                realizedReturn = currI.realizedReturn * currJ.value,
+                unrealizedReturn = currI.unrealizedReturn * currJ.value,
+            )
+
+            res.add(newDataPoint)
+            i++
+        }
+        return UserEquityTimeSeries(
+            res,
+            initialValue * priceData.initialValue,
+            period,
+            currency = currency,
+        )
+    }
+
+    override val currentPrice: Double
+        get() = if (data.isNotEmpty()) {
+            data.last().assetsUnderManagement
+        } else {
+            initialValue
+        }
+
+    fun changeCurrency(exchangeRate: PriceDataSeries, isDivision: Boolean): UserEquityTimeSeries {
+        if (exchangeRate.data.isEmpty()) {
+            return this
+        }
+
+        val res = mutableListOf<EquityDataPoint>()
+
+        var i = 0
+        var j = 0
+        var currI: EquityDataPoint
+        var prevI: EquityDataPoint? = null
+        var currJ: PriceDataPoint
+
+        val baseOperation = { a: Double, b: Double -> if (isDivision) a / b else a * b }
+
+        while (i < data.size) {
+            currI = data[i]
+            currJ = exchangeRate.data[j]
+
+            while (j < exchangeRate.data.size - 1 && exchangeRate.data[j + 1].time.isBefore(currI.time)) {
+                j++
+                currJ = exchangeRate.data[j]
+            }
+
+            val newAssetsUnderManagement = baseOperation(currI.assetsUnderManagement, currJ.value)
+
+            val currInv = currI.assetsUnderManagement - currI.returns
+            val prevInv = prevI?.let { it.assetsUnderManagement - it.returns } ?: currInv
+
+            val prevCalculatedInv = if (res.isEmpty()) prevInv / currJ.value else res.last().assetsUnderManagement - res.last().returns
+            val currCalculatedInv = prevCalculatedInv + (currInv - prevInv) / currJ.value
+
+            val newRealizedReturn = 0.0
+            val newUnrealizedReturn = newAssetsUnderManagement - currCalculatedInv
+
+            val newDataPoint = EquityDataPoint(
+                assetsUnderManagement = newAssetsUnderManagement,
+                value = baseOperation(currI.value, currJ.value),
+                index = currI.index,
+                timeStamp = currI.time.toEpochMilliSecond(),
+                realizedReturn = newRealizedReturn,
+                unrealizedReturn = newUnrealizedReturn
+            )
+
+            res.add(newDataPoint)
+            i++
+            prevI = currI
+        }
+
+        return UserEquityTimeSeries(
+            res,
+            baseOperation(initialValue, exchangeRate.initialValue),
+            period,
+            currency
+        )
+    }
+      fun operation(
+        baseFunction: (Double, Double) -> Double,
+        other: TimeSeries<TimeSeriesDataPoint>
+    ): UserEquityTimeSeries {
+        if (other.data.isEmpty()) {
+            return this
+        }
+
+        val res = mutableListOf<EquityDataPoint>()
+
+        var i = 0
+        var j = 0
+         var currI: EquityDataPoint
+         var currJ: TimeSeriesDataPoint
+
+         val baseOperation = { a: Double, b: Double -> baseFunction(a, b) }
+
+        while (i < data.size) {
+            currI = data[i]
+            currJ = other.data[j]
+
+            while (j < other.data.size - 1 && other.data[j + 1].time.isBefore(currI.time)) {
+                j++
+                currJ = other.data[j]
+            }
+
+            var newAssetsUnderManagement: Double
+            var newRealizedReturn: Double
+            var newUnrealizedReturn: Double
+            if (currJ is EquityDataPoint) {
+                newAssetsUnderManagement = baseOperation(
+                    currI.assetsUnderManagement,
+                    currJ.assetsUnderManagement
+                )
+                newRealizedReturn = baseOperation(
+                    currI.realizedReturn,
+                    currJ.realizedReturn
+                )
+                newUnrealizedReturn = baseOperation(
+                    currI.unrealizedReturn,
+                    currJ.unrealizedReturn
+                )
+            } else {
+                newAssetsUnderManagement = baseOperation(
+                    currI.assetsUnderManagement,
+                    currJ.value
+                )
+                newRealizedReturn = baseOperation(
+                    currI.realizedReturn,
+                    currJ.value
+                )
+                newUnrealizedReturn = baseOperation(
+                    currI.unrealizedReturn,
+                    currJ.value
+                )
+            }
+
+            val newDataPoint = EquityDataPoint(
+                assetsUnderManagement = newAssetsUnderManagement,
+                value = baseOperation(currI.value, currJ.value),
+                index = currI.index,
+                timeStamp = currI.time.toEpochMilliSecond(),
+                realizedReturn = newRealizedReturn,
+                unrealizedReturn = newUnrealizedReturn
+            )
+
+            res.add(newDataPoint)
+            i++
+        }
+
+        return UserEquityTimeSeries(
+            res,
+            baseOperation(initialValue, other.initialValue),
+            period,
+            currency
+        )
+    }
+     override fun toJson(): Map<String, Any> {
+        return mapOf(
+            "data" to data.map { it.toJson() },
+            "period" to period,
+            "initialValue" to initialValue
+        )
+    }
+
+    companion object {
+        fun fromTimeSeries(timeSeries: TimeSeries<TimeSeriesDataPoint>, period: StockDataPeriods): UserEquityTimeSeries {
+            return UserEquityTimeSeries(
+                timeSeries.data as List<EquityDataPoint>,
+                timeSeries.initialValue,
+                period,
+                )
+        }
+
+        fun fromJSON(json: Map<String, Any>): UserEquityTimeSeries {
+            return UserEquityTimeSeries(
+                (json["data"] as List<Map<String, Any>>).map { EquityDataPoint.fromJSON(it) },
+                json["initialValue"] as Double,
+                json["period"] as StockDataPeriods,
+            )
+        }
+    }
+
+
+}
+
+
+
+class EquityDataPoint(
+    val assetsUnderManagement: Double,
+    val realizedReturn: Double,
+    val unrealizedReturn: Double,
+    index: Int,
+    timeStamp: Long,
+    value: Double
+) : TimeSeriesDataPoint(index, timeStamp, value) {
+
+    val returns get() = realizedReturn + unrealizedReturn
+
+    override fun toString(): String {
+        return "{index: $index - time: $time - value: $value - aum: $assetsUnderManagement}"
+    }
+
+    override fun toJson(): Map<String, Any> = mapOf(
+        "d" to time.toEpochMilliSecond(),
+        "index" to index,
+        "indexed" to value,
+        "aum" to assetsUnderManagement,
+        "realized_return" to realizedReturn,
+        "unrealized_return" to unrealizedReturn
+    )
+
+    companion object {
+        fun fromJSON(data: Map<String, Any>): EquityDataPoint {
+            return if (data.containsKey("aum")) {
+                EquityDataPoint(
+                    assetsUnderManagement = data["aum"] as Double,
+                    index = data["index"] as Int,
+                    value = data["indexed"] as Double,
+                    timeStamp = data["d"] as Long,
+                    realizedReturn = data["realized_return"] as? Double ?: 0.0,
+                    unrealizedReturn = data["unrealized_return"] as? Double ?: 0.0
+                )
+            } else {
+                EquityDataPoint(
+                    assetsUnderManagement = data["value"] as Double,
+                    index = data["index"] as Int,
+                    value = data["value"] as Double,
+                    timeStamp = data["d"] as? Long ?: data["timestamp"] as Long,
+                    realizedReturn = data["realized_return"] as? Double ?: 0.0,
+                    unrealizedReturn = data["unrealized_return"] as? Double ?: 0.0
+                )
+            }
+        }
+    }
+
+    fun copyWith(
+        assetsUnderManagement: Double? = null,
+        index: Int? = null,
+        value: Double? = null,
+        timeStamp: Long? = null,
+        realizedReturn: Double? = null,
+        unrealizedReturn: Double? = null
+    ): EquityDataPoint {
+        return EquityDataPoint(
+            assetsUnderManagement ?: this.assetsUnderManagement,
+            realizedReturn ?: this.realizedReturn,
+            unrealizedReturn ?: this.unrealizedReturn,
+            index ?: this.index,
+            timeStamp ?: this.time.toEpochMilliSecond(),
+            value ?: this.value
+        )
+    }
+}
