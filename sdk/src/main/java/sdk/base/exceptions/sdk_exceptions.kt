72696c162451ebed5722fd0e24e44dc307a4b245
Kerem Balic <92550348+KereMBC@users.noreply.github.com>
2023-08-18 06:49:15 -0700

    sdk exceptions added

---
+++ sdk/src/main/java/sdk/base/exceptions/sdk_exceptions.kt
@@ -0,0 +1,15 @@
+package sdk.base.exceptions
+
+open class InitializationException(override val message: String) : Exception() {
+    override fun toString(): String {
+        return "InitializationException: $message"
+    }
+}
+
+class SDKNotInitializedException : InitializationException("SDK not initialized, should call FinfreeSDK.initSDK() first")
+
+class NotAuthorizedException : InitializationException("SDK not authorized for user yet, should call FinfreeSDK.userLogin() or FinfreeSDK.authenticateWithRefreshToken() first")
+
+class CoreDataNotInitializedException : InitializationException("Core data not initialized, should call FinfreeSDK.initializeCoreData() first")
+
+class PortfolioHandlerNotInitializedException : InitializationException("Portfolio handler not initialized, should call FinfreeSDK.initializePortfolioHandler() first")
